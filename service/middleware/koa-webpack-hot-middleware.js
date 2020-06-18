3196684e562114cb9bf7cabad5d6d8d05a1d6bd3
xindong.xie <779788582@qq.com>
2020-06-19 01:04:07 +0800

    111

---
+++ service/middleware/koa-webpack-hot-middleware.js
@@ -0,0 +1,16 @@
+import webpackHotMiddleware from "webpack-hot-middleware";
+import stream from "stream";
+
+export default complimer => {
+    const hotMiddleware = webpackHotMiddleware(complimer, {
+      heartbeat: 1000
+    });
+    //？？？
+    const koaWebpackHotMiddleware = async (ctx, next) => {
+      ctx.body = new stream.PassThrough();
+      await hotMiddleware(ctx.req, ctx.res, next);
+    };
+    koaWebpackHotMiddleware.hotMiddleware = hotMiddleware;
+    return koaWebpackHotMiddleware;
+  };
+  
--- service/utils/dev.js
+++ service/utils/dev.js
@@ -1,1 +1,14 @@
 import webpack from "webpack"
+
+import KoaWebpackDevMiddleware from "../middleware/koa-webpack-dev-middleware"
+import koaWebpackHotMiddleware from "../middleware/koa-webpack-hot-middleware";
+
+const webpackClientConfig = require("../../webpack/webpack.config.dev");
+
+
+
+export default (app,callback)=>{
+
+    const clientCompiler = webpack(webpackClientConfig);
+
+}
